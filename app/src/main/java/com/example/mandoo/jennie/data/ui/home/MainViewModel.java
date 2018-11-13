5405ceaaf36f4efbd177bb923ec9d53e9cc14e3d
Nguyen Tuan Anh E <nguyen.tuan.anhe@framgia.com>
2018-11-13 22:06:23 +0700

    init project

---
+++ app/src/main/java/com/example/mandoo/jennie/data/ui/home/MainViewModel.java
@@ -0,0 +1,8 @@
+package com.example.mandoo.jennie.data.ui.home;
+
+import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableField;
+
+public class MainViewModel extends ViewModel {
+
+}
